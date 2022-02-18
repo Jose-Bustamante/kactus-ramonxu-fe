fd8c8ab5d13036e57010a92d4096021b579bde8f
Jose Bustamante <bercopy@gmail.com>
2022-02-18 16:11:55 +0100

    Material added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { orange } from '@mui/material/colors';
+
+
+import Home from './containers/Home';
+
+const theme = createTheme({
+  status: {
+    danger: orange[500],
+  },
+});
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Ramonxu web app
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+      <Router>
+        <Routes>
+          <Route path="" element={<Home/>} />
+        </Routes>
+      </Router>
+      </ThemeProvider>
+
   );
 }
 
---
+++ src/containers/Home/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      This is the HOME PAGE
+    </>
+  );
+}
+
+export default Home;
